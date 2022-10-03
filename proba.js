02c6c05363e1be902f6d8fe6c5826373a84dda2d
Jelena Lazic <88091526+lazicjelena@users.noreply.github.com>
2022-10-03 13:01:08 +0200

    Add files via upload
    Init commit

---
+++ proba.js
@@ -0,0 +1,21 @@
+function vjezbamojavascript(){
+    //if(a&b) window.open().document.write('RADI SVE SUPER');
+    /*var datum1= document.mojaforma.polazak.getTime();
+    var datum2= document.mojaforma.povratak.getTime();
+    alert(datum1-datum2)*/
+    /*var a='Savaki put kada ucim programiranje zaboravim i sot sam odabrala programiranje kao predet';
+    var b =/java(srcipt)/
+    document.cookie='predmet=programiranje';
+    var kolacici=document.cookie;
+    document.write(kolacici)*/
+    /*var i=1;
+    for(i=0;i<3;i++){
+    var a=document.mojaforma.prodavnica[i].checked;
+    if (a) alert(document.mojaforma.prodavnica[i].value)
+}*/
+       // document.mojaforma.onemogucena.disabled=true;
+       var jmbg=/\d{4}(\s|\-)\d{4}/
+       var datum= new Date
+       var dan=datum.getDate();
+       alert(dan)
+}
